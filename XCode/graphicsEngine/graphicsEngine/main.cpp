6bfde8497fd9f51889c23567943865f9efbcf101
DillTheBeast <dillonmaltese@gmail.com>
2024-04-11 19:50:23 -0400

    More

--- XCode/graphicsEngine/graphicsEngine/main.cpp
+++ XCode/graphicsEngine/graphicsEngine/main.cpp
@@ -1,14 +1,10 @@
-//
-//  main.cpp
-//  graphicsEngine
-//
-//  Created by Dillon Maltese on 4/11/24.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main() {
+    
     return 0;
 }
