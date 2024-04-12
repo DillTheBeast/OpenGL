6884a0e443fd52938cbb8cc998e385b16a457b46
DillTheBeast <dillonmaltese@gmail.com>
2024-04-11 20:02:08 -0400

    Failure

--- build/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dillonmaltese/Documents/GitHub/OpenGL/VS/include/glm/glm
-# Build directory: /Users/dillonmaltese/Documents/GitHub/OpenGL/build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/test/bug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dillonmaltese/Documents/GitHub/OpenGL/VS/include/glm/test/bug
-# Build directory: /Users/dillonmaltese/Documents/GitHub/OpenGL/build/test/bug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test-bug_ms_vec_static "/Users/dillonmaltese/Documents/GitHub/OpenGL/build/test/bug/test-bug_ms_vec_static")
-set_tests_properties(test-bug_ms_vec_static PROPERTIES  _BACKTRACE_TRIPLES "/Users/dillonmaltese/Documents/GitHub/OpenGL/VS/include/glm/test/CMakeLists.txt;231;add_test;/Users/dillonmaltese/Documents/GitHub/OpenGL/VS/include/glm/test/bug/CMakeLists.txt;1;glmCreateTestGTC;/Users/dillonmaltese/Documents/GitHub/OpenGL/VS/include/glm/test/bug/CMakeLists.txt;0;")
